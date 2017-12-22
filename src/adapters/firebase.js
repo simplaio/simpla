7dfe1f4845ef307bf32637b63b0ddfdcfeb9780b
Srinivas Kolluri <kpsrinivas@heresy.in>
2017-12-22 20:05:20 +0530

    Pull request fixes.

--- src/adapters/firebase.js
+++ src/adapters/firebase.js
@@ -1,21 +1,17 @@
 export default class FirebaseAuth{
-    constructor(firebase){
-        this.firebase = firebase;
-    }
+  constructor(firebase){
+    this.firebase = firebase;
+  }
 
-    authenticate(){
-        var provider = new firebase.auth.GithubAuthProvider();
-        provider.addScope('repo');
-        provider.addScope('administration');
-        return this.firebase.auth().signInWithPopup(provider).then(function(result) {
-            var token = result.credential.accessToken;
-            return {token};
-        }).catch(function(error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
-            return false;
-        });
-    }
+  authenticate(){
+    var provider = new this.firebase.auth.GithubAuthProvider();
+    provider.addScope('repo');
+    provider.addScope('administration');
+    return this.firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        return {token};
+    }).catch(function(error) {
+        return false;
+    });
+  }
 }
